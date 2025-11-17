817ed6025f67550d8b8fd85e573214dd7ea2a3b4
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-17 19:50:17 +0000

    shell-scripting

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+person-1=trump
+person-2=musk
+
+echo "$person-1:: hello $person-2"
+echo "$person-2:: hello $person-1 how are you"
+echo "$person-1:: i am good $person-2, whats the ceack"
+echo "$person-2:: just working on my mars mission, you like to go for mars"
+echo "$person-1:: no thank you, i am fine living on the earth"
