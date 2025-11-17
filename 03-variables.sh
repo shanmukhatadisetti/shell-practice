9cfe714b8c0527abff95ebe69c286cda50cfea2e
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-17 19:51:22 +0000

    shell-scripting

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
-person-1=trump
-person-2=musk
+person1=trump
+person2=musk
 
-echo "$person-1:: hello $person-2"
-echo "$person-2:: hello $person-1 how are you"
-echo "$person-1:: i am good $person-2, whats the ceack"
-echo "$person-2:: just working on my mars mission, you like to go for mars"
-echo "$person-1:: no thank you, i am fine living on the earth"
+echo "$person1:: hello $person2"
+echo "$person2:: hello $person1 how are you"
+echo "$person1:: i am good $person2, whats the ceack"
+echo "$person2:: just working on my mars mission, you like to go for mars"
+echo "$person1:: no thank you, i am fine living on the earth"
