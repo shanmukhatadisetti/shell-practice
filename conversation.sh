9e58d3b115345e3c97b99cd9f20c879e3894fa11
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-17 19:46:19 +0000

    shell-scripting

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+echo "trump:: hello musk"
+echo "musk:: hello trump how are you"
+echo "trump:: i am good musk, whats the ceack"
+echo "musk:: just working on my mars mission, you like to go for mars"
+echo "trump:: no thank you, i am fine living on the earth"
