99c2643a199bef55bc8f2e201e91bfe117065228
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-17 23:08:31 +0000

    shell-scripting

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+person1=$1
+person2=$2
+
+echo "$person1:: hello $person2"
+echo "$person2:: hello $person1 how are you"
+echo "$person1:: i am good $person2, whats the ceack"
+echo "$person2:: just working on my mars mission, you like to go for mars"
+echo "$person1:: no thank you, i am fine living on the earth"
