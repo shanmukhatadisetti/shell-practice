155a15ce1c7385c202cfe1a3ffaf511e11940bbd
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-18 13:35:55 +0000

    shell-scripting

--- 08-install-commands.sh
+++ 08-install-commands.sh
@@ -10,4 +10,11 @@ else
     exit 1
 fi
 
-dnf install nginx -y
+dnf install nnginxertyui -y
+if [ $? -eg 0 ]
+then
+    echo "installation succesful"
+else
+    echo "ERROR:: installation has been failed"
+    exit 1
+fi
