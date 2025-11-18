cef8b778dcfdd26675298261f2475acbcb85cc40
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-18 18:28:08 +0000

    shell-scripting

--- 11-creating-aws.sh
+++ 11-creating-aws.sh
@@ -2,14 +2,14 @@ ami_id="ami-09c813fb71547fc4f"
 security_group="sg-061a70ce669d05c8c"
 instance_name=("mongodb" "catalogue" "frontend")
 
-for instances in ${instance_name[@]}
+for instance in ${instance_name[@]}
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-061a70ce669d05c8c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instances}]" --query "Instances[0].InstanceId" --output text)
-    # if [ $instance_name != "frontend" ]
-    # then
-    #     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-    # else
-    #     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-    # fi
-    # echo "$instances ip-address is:: $IP"
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-061a70ce669d05c8c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
+    echo "$instances ip-address is:: $IP"
 done
