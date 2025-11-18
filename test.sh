cc46eb989225407bde763f58f7e4ed9b98657b0d
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-18 18:49:09 +0000

    shell-scripting

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ instance_name=("mongodb" "catalogue" "frontend")
 
 for instance in ${instance_name[@]}
 do
-    INSTANCES4=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-061a70ce669d05c8c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=shan}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCES4=$(run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-061a70ce669d05c8c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=shan}]" --query "Instances[0].InstanceId" --output text)
     echo $INSTANCES4
 done
