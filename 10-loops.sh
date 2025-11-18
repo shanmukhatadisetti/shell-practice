06560fd7e0018715c6a264b17fa45479a48e55ed
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-18 16:19:08 +0000

    shell-scripting

---
+++ 10-loops.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
+
+SCRIPT_PATH=/var/log/scripting_logs
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$SCRIPT_PATH/$SCRIPT_NAME.log"
+
+mkdir -p $SCRIPT_PATH
+echo "script started excuting at:: $(date)"
+
+PACKAGES=("mysql" "nginx" "python3")
+
+if [ $userid -eq 0 ]
+then
+    echo -e "$G your running with root user $W" | tee -a $LOG_FILE
+else
+    echo -e "$R ERROR:: your not running with root user $W" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATION(){
+        if [ $1 -eq 0 ]
+    then
+        echo -e "$G $2 installation succesful $W" | tee -a $LOG_FILE
+    else
+        echo -e "$R ERROR:: $2 installation has been failed $W" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+for packages in $PACKAGES
+do 
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then 
+        echo "The package is installing" &>>$LOG_FILE
+         install $package -y &>>$LOG_FILE
+        VALIDATION $? "$package"
+
+    else
+    echo -e "$Y $package package is already installed $W" | tee -a $LOG_FILE
+fi
+done
+
+
