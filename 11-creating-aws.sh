f43a16dce2c10d3cb645c2af380faae8c2ee949b
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-18 18:15:51 +0000

    shell-scripting

--- 11-creating-aws.sh
+++ 11-creating-aws.sh
@@ -7,9 +7,9 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $security_group --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]" --query 'Instances[0].InstanceId' --output text)
     if [ $instance_name != "frontend" ]
     then
-        ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
-        ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
     echo "$instances ip-address is:: $ip"
 done
