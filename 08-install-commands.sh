005317f125b7ef27779fc7b8fe8270b45eabf2ca
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-18 14:23:50 +0000

    shell-scripting

--- 08-install-commands.sh
+++ 08-install-commands.sh
@@ -9,18 +9,46 @@ else
     echo "ERROR:: your not running with root user"
     exit 1
 fi
+
+VALIDATION(){
+        if [ $1 -eq 0 ]
+    then
+        echo "$2 installation succesful"
+    else
+        echo "ERROR:: $2 installation has been failed"
+        exit 1
+    fi
+}
+
 dnf list installed nginx
 if [ $? -ne 0 ]
 then 
     echo "The package is installing"
     dnf install nginx -y
-    if [ $? -eq 0 ]
-    then
-        echo "installation succesful"
-    else
-        echo "ERROR:: installation has been failed"
-        exit 1
-    fi
+    VALIDATION $? "nginx"
+
+else
+    echo "The package is already installed"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+    echo "The package is installing"
+    dnf install python3 -y
+    VALIDATION $? "python3"
+
+else
+    echo "The package is already installed"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo "The package is installing"
+    dnf install mysql -y
+    VALIDATION $? "mysql"
+
 else
     echo "The package is already installed"
 fi
