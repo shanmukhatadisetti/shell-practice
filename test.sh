f489933e55eae75254ac7264aba563ba0a27fa96
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-18 20:05:41 +0000

    shell-scripting

--- test.sh
+++ test.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
-export PATH=$PATH:/usr/local/bin
+
+# ami_id="ami-09c813fb71547fc4f"
+# security_group="sg-061a70ce669d05c8c"
+# instance_name=("mongodb" "catalogue" "frontend")
+
+# for instance in ${instance_name[@]}
+# do
+#     INSTANCE_ID=$(aws ec2 run-instances --image-id $ami_id --instance-type t2.micro --security-group-ids $security_group --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+#     if [ $instance != "frontend" ]
+#     then 
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+#     else
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+#     fi
+#     echo "$instance ip address is:: $IP"
+# done
+
 ami_id="ami-09c813fb71547fc4f"
 security_group="sg-061a70ce669d05c8c"
-instance_name=("mongodb" "catalogue" "frontend")
+instance_name=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 
-for instance in ${instance_name[@]}
-do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $ami_id --instance-type t2.micro --security-group-ids $security_group --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != "frontend" ]
-    then 
+for instances in ${instance_name[@]}
+do 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-061a70ce669d05c8c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instances}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instances != "frontend" ]
+    then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
-    echo "$instance ip address is:: $IP"
+    echo "$instances ip-address is:: $IP"
 done
