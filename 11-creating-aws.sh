b6ae5127fefb840bf50117db64280715150d9edf
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-18 18:06:21 +0000

    shell-scripting

---
+++ 11-creating-aws.sh
@@ -0,0 +1,15 @@
+ami_id="ami-09c813fb71547fc4f"
+security_group="sg-061a70ce669d05c8c"
+instance_name=("mongodb" "catalogue" "frontend")
+
+for instances in ${instance_name[@]}
+do 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $security_group --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}" --query 'Instances[0].InstanceId' --output text)
+    if [ $instance_name != "frontend" ]
+    then
+        ip=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+        ip=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+    echo "$instances ip-address is:: $ip"
+done
