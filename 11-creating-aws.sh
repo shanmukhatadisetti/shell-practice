630994e504d6d8497709158f9a13de10ddb5334b
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-18 18:20:52 +0000

    shell-scripting

--- 11-creating-aws.sh
+++ 11-creating-aws.sh
@@ -5,11 +5,11 @@ instance_name=("mongodb" "catalogue" "frontend")
 for instances in ${instance_name[@]}
 do 
     INSTANCE_ID=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $security_group --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance_name != "frontend" ]
-    then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-    else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-    fi
-    echo "$instances ip-address is:: $IP"
+    # if [ $instance_name != "frontend" ]
+    # then
+    #     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    # else
+    #     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    # fi
+    # echo "$instances ip-address is:: $IP"
 done
