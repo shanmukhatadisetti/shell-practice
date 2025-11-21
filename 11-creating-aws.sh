03408f8583ca5f1818d9822559636a9059996a37
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-21 11:43:27 +0000

    shell-scripting

--- 11-creating-aws.sh
+++ 11-creating-aws.sh
@@ -33,7 +33,7 @@ do
                 ,"Type"             : "A"
                 ,"TTL"              : 1
                 ,"ResourceRecords"  : [{
-                "Value"         : "' $IP '"
+                "Value"         : "'$IP'"
             }]
         }
         }]
